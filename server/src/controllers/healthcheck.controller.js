905ece4604e031ba26f80041934eb3a76f016248
Pritam <pritambose040@gmail.com>
2024-09-05 21:36:08 +0530

    Add Healthcheck

--- server/src/controllers/healthcheck.controller.js
+++ server/src/controllers/healthcheck.controller.js
@@ -2,7 +2,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-  return resstatus(200).json(new ApiResponse(true, null, "Health check OK"));
+  return res.status(200).json(new ApiResponse(true, null, "Health check OK"));
 });
 
 export { healthcheck };
