03b50700bbab76462056847c3c541cd43e1a24e9
Pritam <pritambose040@gmail.com>
2024-09-06 00:13:56 +0530

    Change vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const baseUrl = String(process.env.VITE_API_BASE_URL_DEV);
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api": import.meta.env.VITE_API_BASE_URL_DEV,
+      "/api": baseUrl || "http://localhost:3000",
     },
     changeOrigin: true,
     secure: false,
