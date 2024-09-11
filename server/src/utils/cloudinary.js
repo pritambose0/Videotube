6b2a83b1a793ff3ae109edafc9d7e1714df98fc4
Pritam <pritambose040@gmail.com>
2024-09-11 23:52:18 +0530

    Minor Update

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -14,6 +14,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     //Upload the file on Cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
+      secure: true,
     });
     // console.log("Cloudinary Response - ", response);
 
