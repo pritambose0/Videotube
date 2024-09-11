019eb2cc2e40d5ce80ee4b1d57bfee129cab0543
Pritam <pritambose040@gmail.com>
2024-09-11 23:45:16 +0530

    Minor Update

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -5,6 +5,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUDNAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
